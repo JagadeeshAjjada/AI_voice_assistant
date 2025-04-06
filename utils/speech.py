adf5778f5f122b0ccddfed8a40c79e0050b0041b
JagadeeshAjjada <jagadeesh.ajjada493@gmail.com>
2025-04-05 21:11:09 -0700

    Speech code changes

--- utils/speech.py
+++ utils/speech.py
@@ -1,17 +1,16 @@
 import speech_recognition as sr
 import pyttsx3
 
-def record_audio(prompt=""):
-    r = sr.Recognizer()
+def record_audio(timeout=5, phrase_time_limit=5):
+    recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        print(prompt)
-        audio = r.listen(source, timeout=5, phrase_time_limit=5)
-    try:
-        return r.recognize_google(audio)
-    except:
-        return "Sorry, I didn’t catch that."
+        try:
+            audio = recognizer.listen(source, timeout=timeout, phrase_time_limit=phrase_time_limit)
+            return recognizer.recognize_google(audio)
+        except Exception as e:
+            return ""
 
-def engine_speak(text):
+def speak_text(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
